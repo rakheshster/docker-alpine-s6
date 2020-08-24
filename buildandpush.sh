c39515fbe286a163933e871e3a59e09656f0acb8
Rakhesh Sasidharan <me@rakhesh.com>
2020-08-24 18:49:47 +0100

    Added progress indicator

--- buildandpush.sh
+++ buildandpush.sh
@@ -3,4 +3,4 @@
 ALPINE_VERSION=3.12
 S6_VERSION=2.0.0.1
 
-docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t rakheshster/alpine-s6:${ALPINE_VERSION}-${S6_VERSION}
+docker buildx build --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6 . --push -t rakheshster/alpine-s6:${ALPINE_VERSION}-${S6_VERSION} --progress=plain
