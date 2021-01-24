61f4134ffe8f98ea186d830b801e90f1b12a48e3
Rakhesh Sasidharan <me@rakhesh.com>
2021-01-24 15:10:45 +0000

    tidied up my helper scripts

--- buildlocal.sh
+++ buildlocal.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-# Usage ./buildandpush.sh
-
-# Not all platforms have all archs if I am building locally. For example M1 Macs don't do linux/386 anymore. 
-# ARCH="linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6"
-# ARCH="linux/amd64,linux/arm64,linux/arm/v7,linux/arm/v6"
-ARCH="linux/amd64,linux/arm64"
+# Usage ./buildlocal.sh 
 
 BUILDINFO="$(pwd)/buildinfo.json"
 if ! [[ -r "$BUILDINFO" ]]; then echo "Cannot find $BUILDINFO file. Exiting ..."; exit 1; fi
@@ -14,11 +9,10 @@ if ! command -v jq &> /dev/null; then echo "Cannot find jq. Exiting ..."; exit 1
 VERSION=$(jq -r '.version' $BUILDINFO)
 IMAGENAME=$(jq -r '.imagename' $BUILDINFO)
 
-docker buildx build --platform $ARCH -t ${IMAGENAME}:${VERSION} -t ${IMAGENAME}:latest --progress=plain .
+# delete an existing image of the same name if it exists
+# thanks to https://stackoverflow.com/questions/30543409/how-to-check-if-a-docker-image-with-a-specific-tag-exist-locally
+if [[ $(docker image inspect ${IMAGENAME} 2>/dev/null) == "" ]]; then
+    docker rmi -f ${IMAGENAME}:${VERSION}
+fi
 
-echo ""
-echo "Loading the image of the current architecture (this could fail if I didn't specify it in the ARCH variable earlier)"
-docker buildx build --load -t ${IMAGENAME}:${VERSION} -t ${IMAGENAME}:latest .
-
-# See https://github.com/docker/buildx/issues/59 for the background of the --load switch above
-# You use --push on multi-platform and --load for single platform
+docker build -t ${IMAGENAME}:${VERSION} -t ${IMAGENAME}:latest .
